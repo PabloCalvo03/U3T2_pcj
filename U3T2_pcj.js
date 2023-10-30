1b43bb388453aff1afe6446eefffcffe636ef29c
Pablo <pablocalvoj@gmail.com>
2023-10-30 13:45:49 +0100

    terminandolo

--- U3T2_pcj.js
+++ U3T2_pcj.js
@@ -1,4 +1,17 @@
-const socios = [];
+function Socio(numSocio, dni, nombre, apellidos, fechaNacimiento, localidad){
+    this.numSocio = numSocio;
+    this.dni = dni;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.fechaNacimiento = fechaNacimiento;
+    this.localidad = localidad;
+
+    this.modificarLocalidad = (localidad) => {
+        this.localidad = localidad;
+    }
+}
+
+let socios = [];
 
 const crearSocio = () => {
     const numSocio = prompt("Introduce el numero de socio")
@@ -9,9 +22,21 @@ const crearSocio = () => {
     const localidad = prompt("Introduce la localidad del socio");
     const nuevoSocio = new Socio(numSocio, dni, nombre, apellidos, fechaNacimiento, localidad);
     socios.push(nuevoSocio);
+    alert(`Socio ${nuevoSocio.nombre} con dni ${nuevoSocio.dni} creado con exito`)
 }
 
 const mostrarSocios = () => {
+    let htmlConstruido = "";
+    for(let i = 0; i < socios.length; i++){
+        htmlConstruido += `<tr>
+        <td>${socios[i].numSocio}</td>
+        <td>${socios[i].dni}</td>
+        <td>${socios[i].nombre}</td>
+        <td>${socios[i].apellidos}</td>
+        <td>${socios[i].fechaNacimiento}</td>
+        <td>${socios[i].localidad}</td>
+        </tr>`
+    }
     document.write(`<table>
         <thead>
             <tr>
@@ -24,54 +49,43 @@ const mostrarSocios = () => {
             </tr>
         </thead>
         <tbody>
-            ${socios.map(socio => {
-                return ( 
-                `<tr>
-                    <td>${socio.numSocio}</td>
-                    <td>${socio.dni}</td>
-                    <td>${socio.nombre}</td>
-                    <td>${socio.apellidos}</td>
-                    <td>${socio.fechaNacimiento}</td>
-                    <td>${socio.localidad}</td>
-                </tr>`
-                )
-            })}
+            ${htmlConstruido}
         </tbody>
     </table>`)
 }
 
 const darDeBajaSocio = () => {
     const eleccion = prompt("1 - Eliminar por DNI\n2 - Eliminar por numero de socio");
+    let indiceABorrar = -1;
     switch(eleccion){
         case "1":
             const dni = prompt("Introduce el dni del socio")
-            socios.forEach((socio, index)=> {
-                if(socio.dni = dni){
-                    delete(socios[index])
+            for (let i = 0; i < socios.length; i++){
+                if(socios[i].dni == dni){
+                    indiceABorrar = i;
                 }
-            })
+            }
+            if(indiceABorrar != -1){
+                const socioBorrado = socios.splice(indiceABorrar, 1)
+                alert(`Socio ${socioBorrado[0].nombre} con dni ${socioBorrado[0].dni} borrado con exito`);
+            } else {
+                alert("No se ha encontrado al socio");
+            }
         break;
         case "2":
-            const numSocio = prompt("Introduce el numero de socio")
-            socios.forEach((socio, index)=> {
-                if(socio.numSocio = numSocio){
-                    delete(socios[index])
+            const numSocio = prompt("Introduce el numero del socio")
+            for (let i = 0; i < socios.length; i++){
+                if(socios[i].numSocio == numSocio){
+                    indiceABorrar = i;
                 }
-            })
+            }
+            if(indiceABorrar != -1){
+                socios.splice(indiceABorrar, 1);
+                alert(`Socio ${socioBorrado[0].nombre} con dni ${socioBorrado[0].dni} borrado con exito`);
+            } else {
+                alert("No se ha encontrado al socio");
+            }
         break;
-    }
-}
-
-function Socio(numSocio, dni, nombre, apellidos, fechaNacimiento, localidad){
-    this.numSocio = numSocio;
-    this.dni = dni;
-    this.nombre = nombre;
-    this.apellidos = apellidos;
-    this.fechaNacimiento = fechaNacimiento;
-    this.localidad = localidad;
-
-    this.modificarLocalidad = (localidad) => {
-        this.localidad = localidad;
     }
 }
 
